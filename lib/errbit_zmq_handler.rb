7b8686e79c5b7f9f637a4d97910d5b349a469502
Nick Recobra <oruen@undev.ru>
2011-02-28 17:19:19 +0300

    Using inheritance for handler definition.

--- lib/errbit_zmq_handler.rb
+++ lib/errbit_zmq_handler.rb
@@ -5,15 +5,13 @@ require 'dripdrop'
 Thread.abort_on_exception = true
 
 module ErrbitZmqHandler
-  class << self
-    def run!
-      DripDrop::Node.new do
-        route :exceptions, :zmq_subscribe, "tcp://127.0.0.1:9998", :bind, :msg_format => :raw
+  class Handler < DripDrop::Node
+    def action 
+      route :exceptions, :zmq_subscribe, "tcp://127.0.0.1:9998", :bind, :msg_format => :raw
       
-        exceptions.on_recv :raw do |messages|
-          puts "Receiver 1: #{messages.first.copy_out_string}"
-        end
-      end.start!      
+      exceptions.on_recv do |messages|
+        puts "Receiver 1: #{messages.first.copy_out_string}"
+      end
     end
   end
 end

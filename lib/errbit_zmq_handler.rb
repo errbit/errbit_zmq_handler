a80fceb7e5221bf3c415898ce8de0f8527475bb8
Nick Recobra <oruen@undev.ru>
2011-02-28 11:01:03 +0300

    ErrbitZmqHandler.run! для запуска хендлера.

--- lib/errbit_zmq_handler.rb
+++ lib/errbit_zmq_handler.rb
@@ -1,15 +1,20 @@
 # encoding: utf-8
 require 'yajl'
 require 'dripdrop'
-module ErrbitZmqHandler
-end
 
 Thread.abort_on_exception = true
 
-DripDrop::Node.new do
-  route :exceptions, :zmq_subscribe, "tcp://127.0.0.1:9998", :bind, :msg_format => :raw
-
-  exceptions.on_recv :raw do |messages|
-    puts "Receiver 1: #{messages.first.copy_out_string}"
+module ErrbitZmqHandler
+  class << self
+    def run!
+      DripDrop::Node.new do
+        route :exceptions, :zmq_subscribe, "tcp://127.0.0.1:9998", :bind, :msg_format => :raw
+      
+        exceptions.on_recv :raw do |messages|
+          puts "Receiver 1: #{messages.first.copy_out_string}"
+        end
+      end.start!      
+    end
   end
-end.start!
+end
+
